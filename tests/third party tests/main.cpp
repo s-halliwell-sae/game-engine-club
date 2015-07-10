20f518f6a8ddb82385ee08ae0de52c617fb950ab
Steve Halliwell <halliwell.steve@gmail.com>
2015-07-10 15:15:35 +1000

    Started project and placed 3rd party test todo in solution

---
+++ tests/third party tests/main.cpp
@@ -0,0 +1,5 @@
+/*TODO
+	generic property sheet with all 3rd party deps included and linked etc.
+	use each 3rd party lib in at least a hello world style to confirm that they are there and working
+
+*/
