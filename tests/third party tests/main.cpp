38f5d319b337cc01d973631c85ec10ec5aa1f32b
maesty-pc <halliwell.steve@gmail.com>
2015-07-15 09:10:31 +1000

    Added thridparty props sheet main compiles and knows about spdlog, glm, lua. Has not been built yet, lua dlls need to move to output folder and we don't know what that is yet

--- tests/third party tests/main.cpp
+++ tests/third party tests/main.cpp
@@ -3,3 +3,17 @@
 	use each 3rd party lib in at least a hello world style to confirm that they are there and working
 
 */
+
+#include "lua.hpp"
+#include "glm.hpp"
+#include "spdlog.h"
+
+void main()
+{
+	glm::vec3 a,b;
+
+	a = a + b;
+
+	auto console = spdlog::stdout_logger_mt("console");
+	console->info("Welcome to spdlog!");
+}
